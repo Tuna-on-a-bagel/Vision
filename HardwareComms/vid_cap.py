67b84ccf04a1080da31650a283eec2b3657c27ee
Paul D <51982197+Tuna-on-a-bagel@users.noreply.github.com>
2023-01-20 11:04:42 -0500

    Update vid_cap.py

--- HardwareComms/vid_cap.py
+++ HardwareComms/vid_cap.py
@@ -24,7 +24,7 @@ class vid_capture:
     def vid_feed(self):
         '''A function to switch between ssh feed, webcam feed, and a video file read
         -------------------
-        source = 'ssh' or 'onboard cam' or 'fromFile'
+        source = 'ssh' or 'onboard' or 'fromFile'
         '''
         if (self.source == 'ssh') or (self.source == 'SSH'):
             self.jetson, self.frame = self.cap.recv_image()
@@ -39,26 +39,6 @@ class vid_capture:
                 return self.ret, self.frame
             else: print('no returned image')
 
-'''
-image_hub = imagezmq.ImageHub()
-#connected = False
-count = 0
-while True:  # show streamed images until Ctrl-C
-    jetson, image = image_hub.recv_image()
-    if jetson:
-        connected = True
-        cv2.imshow(jetson, image) # 1 window for each RPi
-        cv2.waitKey(1)
-        image_hub.send_reply(b'OK')
-
-    if connected:
-        count += 1
-        
-        if count > 500:
-            break
-
-cv.destroyAllWindows()
-'''
 
 if __name__ == "__main__":
     vis = vid_capture("onBoard")
